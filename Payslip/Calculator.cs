15f35c64e8417a1c0513f7234bf49c9635dd594a
Amrit Kaur <Amrit.Kaur@myob.com>
2017-11-23 12:08:16 +1300

    added payslip and calc files

---
+++ Payslip/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Payslip
+{
+    internal class Calculator
+    {
+        internal string combineFirstAndLastName(string fname, string lname)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Payslip/CalculatorShould.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Payslip
+{
+    [TestClass]
+    public class CalculatorShould
+    {
+        [TestMethod]
+        public void FailNameTest()
+        {
+            string fname = "Me";                    //inputs
+            string lname = "Me";
+
+            Calculator eg = new Calculator(); // create a class for the logic
+            string name = eg.combineFirstAndLastName(fname, lname);
+            // string name = fname + lname; -- logic shouldnt be part of test
+
+            Assert.AreEqual("MeMe", name);
+        }
+
+        [TestMethod]
+        public void anotherTest()
+        {
+
+        }
+
+
+    }
+}
