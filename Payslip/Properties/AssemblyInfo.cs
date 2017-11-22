18df288c82fbb534e9a5b7ee4151659c4301bcac
Amrit Kaur <Amrit.Kaur@myob.com>
2017-11-23 10:51:41 +1300

    Add project files.

---
+++ Payslip/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Payslip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MYOB")]
+[assembly: AssemblyProduct("Payslip")]
+[assembly: AssemblyCopyright("Copyright © MYOB 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf8b75b4-529a-461f-ac18-54807465a77f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
