8b0d843e95913b17e97ea535a678e98203ce21e1
Amrit Kaur <Amrit.Kaur@myob.com>
2017-11-23 15:33:47 +1300

    added tests for taxincome, netincome, super

--- Payslip/CalculatorShould.cs
+++ Payslip/CalculatorShould.cs
@@ -27,7 +27,7 @@ namespace Calculator
 
         /*
         [TestMethod]
-        public void JunkGIncome()
+        public void TestGIncome()
         {
             int salary = 18000;
             PayCalculator Income = new PayCalculator();
@@ -46,8 +46,67 @@ namespace Calculator
 
             Assert.AreEqual(19, tIncome);
         }
-
         
+        /*
+        [TestMethod]
+        public void TestTIncome()
+        {
+            double income = 150000;
+            PayCalculator tax = new PayCalculator();
+            double tIncome = tax.calculateIncomeTax(income);
 
+            Assert.AreEqual(43447, tIncome);
+        }
+        */
+
+        [TestMethod]
+        public void NIncome()
+        {
+            double gIncome = 1000;
+            double tIncome = 100;
+
+            PayCalculator netIncome = new PayCalculator();
+            double nIncome = netIncome.calculateNetIncome(gIncome,tIncome);
+
+            Assert.AreEqual(900, nIncome);
+        }
+
+        /*
+        [TestMethod]
+        public void TestNIncome()
+        {
+            double gIncome = 2000;
+            double tIncome = 500;
+
+            PayCalculator netIncome = new PayCalculator();
+            double nIncome = netIncome.calculateNetIncome(gIncome, tIncome);
+
+            Assert.AreEqual(1500, nIncome);
+        }
+        */
+
+        [TestMethod]
+        public void Super()
+        {
+            double rate = 9;
+            double netIncome = 5000;
+            
+            PayCalculator super = new PayCalculator();
+            double superRate = super.calculateSuperRate(rate, netIncome);
+
+            Assert.AreEqual(450, superRate);
+        }
+
+        [TestMethod]
+        public void TestSuper()
+        {
+            double rate = 7;
+            double netIncome = 6000;
+
+            PayCalculator super = new PayCalculator();
+            double superRate = super.calculateSuperRate(rate, netIncome);
+
+            Assert.AreEqual(420, superRate);
+        }
     }
 }
--- Payslip/PayCalculator.cs
+++ Payslip/PayCalculator.cs
@@ -19,10 +19,52 @@ namespace Calculator
 
         internal double calculateIncomeTax(double income)
         {
-            double Income = income - 18200;
-            double result = 0.19 * Income;
+            double result = 0;
+            double Income;
+
+            if (18200 > income)
+            {
+                result = 0;
+            }
+
+            else if (18200 < income && income < 37000)
+            {
+                Income = income - 18200;
+                result = 0.19 * Income;
+            }
+            else if (37000 < income && income < 80000)
+            {
+                Income = income - 37000;
+                result = (0.325 * Income) + 3575;
+            }
+            else if (80000 < income && income < 180000)
+            {
+                Income = income - 80000;
+                result = (0.37 * Income) + 17547;
+            }
+            else 
+            {
+                Income = income - 180000;
+                result = (0.45 * Income) + 54547;
+            }
+            
             return result;
             //throw new NotImplementedException();
+        }
+
+        internal double calculateNetIncome(double gIncome, double tIncome)
+        {
+            double incomeNet = gIncome - tIncome;
+
+            return incomeNet;
+            //throw new NotImplementedException();
+        }
+
+        internal double calculateSuperRate(double rate, double netIncome)
+        {
+            double result = netIncome * (rate / 100);
+
+            return result;
         }
     }
 }
